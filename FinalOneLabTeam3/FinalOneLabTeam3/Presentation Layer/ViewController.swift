67db85ea4e2d2c31cfc0bc7b7ba3fae3d1474fdf
Bakdaulet <berdikulbakdaulet@gmail.com>
2022-04-25 22:25:56 +0600

    Default configuration, added Podfile with SnapKit and Alamofire.

---
+++ FinalOneLabTeam3/FinalOneLabTeam3/Presentation Layer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalOneLabTeam3
+//
+//  Created by Bakdaulet Berdikul on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Final Project"
+    }
+
+
+}
+
